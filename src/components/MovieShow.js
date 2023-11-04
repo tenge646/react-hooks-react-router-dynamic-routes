5ee8c4f6a51c0146c300ff9eac76384f75b5ea67
voltrager <voltrager@kinsman.voltrager.org>
2023-11-04 12:32:01 +0300

    compeled assignment

---
+++ src/components/MovieShow.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function MovieShow({ movies }) {
+  const { movieId } = useParams();
+  const selectedMovie = movies.find((movie) => movie.id === parseInt(movieId));
+
+  return (
+    <div>
+      {selectedMovie ? (
+        <div>
+          <h3>{selectedMovie.title}</h3>
+          {/* Add other movie details here */}
+        </div>
+      ) : (
+        <div>Movie not found</div>
+      )}
+    </div>
+  );
+}
+
+export default MovieShow;
--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route path={`${match.path}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
+
 export default MoviesPage;
